2108ceba07f7b63817e4a7cb491cbac351d0d472
Balaji Ramkumar <balaji512ramkumar@gmail.com>
2017-10-15 21:10:35 +0200

    final

--- Lab7/R/pred.R
+++ Lab7/R/pred.R
@@ -5,13 +5,10 @@ library(nycflights13)
 library(caret)
 
 
-# reading data 
-
 flights1 <- nycflights13::flights
 
 weather1 <- nycflights13::weather
 
-# Data prep
 
 weather1 <- weather1 %>%
   
@@ -36,24 +33,18 @@ weathna
 lapply(datfram1, FUN = function(x){summary(x)})
 
 
-# removing all values which do not have effect on the modelling
 
 datfram1 <- datfram1[complete.cases(datfram1), ] %>%
   
   dplyr::select(-year)
 
 
-# Reducing the amount of data used for analysis
 
 datfram1 <- datfram1 %>%
   
   sample_frac(.1)
 
 
-
-
-
-# selecting data we thing is good enough for modelling 
 
 cor_matrix <- cor(datfram1[sapply(datfram1, is.numeric)])
 
@@ -66,11 +57,6 @@ has_effect <- cor_matrix[cor_matrix[,"arr_delay"] >= abs(0.15) ,"arr_delay"]
 formula_short <- arr_delay ~ dep_delay + dep_time + hour
 
 
-
-
-
-# removing unecessary data 
-
 datfram1 <- datfram1 %>%
   
   dplyr::select(-c(sched_dep_time, carrier, flight, tailnum, dest,
@@ -78,7 +64,6 @@ datfram1 <- datfram1 %>%
                    minute, time_hour, wind_gust))
 
 
-# Creating test and training data
 
 traindat<- createDataPartition(y = datfram1$arr_delay, 
                                 
@@ -91,10 +76,6 @@ traindat<- createDataPartition(y = datfram1$arr_delay,
 training <- datfram1[traindat, ]
 
 tmp <- datfram1[-traindat, ]
-
-
-
-# Creating test data that is valid
 
 validat<- createDataPartition(y = tmp$arr_delay, p = .75, list = FALSE)
 
@@ -110,24 +91,10 @@ rm(tmp)
 
 
 
-
-
-# Sampling method is set 
-
-
 resamp<- trainControl(method = "repeatedcv", 
                      
                      repeats = 3)
 
-
-
-
-
-# Creating models
-
-
-
-# Model 1
 
 start <- Sys.time()
 
@@ -145,17 +112,8 @@ end <- Sys.time()
 
 end-start
 
-
-
-
-
-# Predicting
-
 lmr_class1 <- predict(lmr_fit1, newdata = validate)
 
-
-
-# Evaluation
 
 lmr_fit1
 
@@ -168,8 +126,6 @@ df_lmr_fit1 <- validate %>%
   cbind(., fitted_values = predict(lmr_fit1, newdata = validate)) %>%
   
   mutate(resid = arr_delay - fitted_values)
-
-
 
 
 
@@ -192,13 +148,6 @@ gg2_lmr_fit1 <- ggplot(data = df_lmr_fit1, aes(x = fitted_values, y = resid)) +
 postResample(lmr_class1, validate$arr_delay)
 
 
-
-
-
-
-
-# second model 
-
 lmr_fit2 <- train(formula_short,
                   
                   data = training,
@@ -210,12 +159,7 @@ lmr_fit2 <- train(formula_short,
                   preProc = "scale")
 
 
-
-# Predicting
-
 lmr_class2 <- predict(lmr_fit2, newdata = validate)
-
-
 
 
 lmr_fit2
@@ -229,9 +173,6 @@ df_lmr_fit2 <- validate %>%
   cbind(., fitted_values = predict(lmr_fit2, newdata = validate)) %>%
   
   mutate(resid = arr_delay - fitted_values)
-
-
-
 
 
 gg1_lmr_fit2 <- ggplot(data = df_lmr_fit2, aes(x = arr_delay, y = fitted_values)) +
@@ -253,11 +194,6 @@ gg2_lmr_fit2 <- ggplot(data = df_lmr_fit2, aes(x = fitted_values, y = resid)) +
 postResample(lmr_class2, validate$arr_delay)
 
 
-
-# capable model
-
-
-
 resamp <- resamples(list(rreg_all_vars = lmr_fit1,
                          
                          rreg_high_cor = lmr_fit2))
@@ -265,5 +201,3 @@ resamp <- resamples(list(rreg_all_vars = lmr_fit1,
 
 
 summary(resamp)
-
-# End of code
